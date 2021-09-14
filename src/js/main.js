777fd186956967ee19ed8da6bf0033516440be4a
Vladimir Sergeichik <support@triare.net>
2021-09-14 15:27:04 +0300

    update structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 const addButton = document.querySelector('.card1');
+
 let today  = new Date().toLocaleString('en',
 {
   minute: 'numeric',
-  hour: 'numeric',  
+  hour: 'numeric',
   day: 'numeric',
   month: 'long',
   year: 'numeric'
 });
+
 let a = "Title example"
 let b = "Text example"
-
-
 
 function createCardTitle(title, parent) {
     const conteiner = document.createElement('div');
@@ -18,7 +18,7 @@ function createCardTitle(title, parent) {
     const h3 = document.createElement('h3');
     h3.innerText = title;
     conteiner.appendChild(h3);
-    
+
 
     // const view = document.createElement('button');
     // view.className = 'but';
@@ -27,9 +27,9 @@ function createCardTitle(title, parent) {
     // view.addEventListener('click', (e) => {
     //     e.preventDefault();
 
-        
+
     // }, false);
-    
+
     const edit = document.createElement('button');
     edit.className = 'but';
     edit.innerHTML = '<i class="fa fa-pencil" aria-hidden="true"></i>';
@@ -37,9 +37,9 @@ function createCardTitle(title, parent) {
     edit.addEventListener('click', (e) => {
         e.preventDefault();
 
-        
+
     }, false);
-    
+
     const drop = document.createElement('button');
     drop.className = 'but';
     drop.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
@@ -67,7 +67,7 @@ function createCard(title, text, info) {
     meta.className = 'card-meta';
     meta.innerText = info;
     conteiner.appendChild(meta);
-    
+
     return conteiner;
 }
 
@@ -75,12 +75,12 @@ function test() {
     [
         {
             title: 'Simple Note',
-            text: 'Сьогодні хороший день і все добре загалом тааааккккі dsadasdasddddddd ddddddd dddddddddddddddd dddddddddddddd saddddddddddddd  dwqwqq віфввввввввввввіфвцй уйц кйцкйцкцквівфівфівфівіфв gergreget e tertert er tretret erterter  wer werwer werwerewrwer wrewerew reewrewr ewrwewrttr retrtrt erytrey ytyі', 
+            text: 'Сьогодні хороший день і все добре загалом тааааккккі dsadasdasddddddd ddddddd dddddddddddddddd dddddddddddddd saddddddddddddd  dwqwqq віфввввввввввввіфвцй уйц кйцкйцкцквівфівфівфівіфв gergreget e tertert er tretret erterter  wer werwer werwerewrwer wrewerew reewrewr ewrwewrttr retrtrt erytrey ytyі',
             info: 'PUBLISH: ' + today
         }
         // {
         //     title: 'Simple Note',
-        //     text: b, 
+        //     text: b,
         //     info: 'PUBLISH: ' + today
         // }
     ].forEach(({
